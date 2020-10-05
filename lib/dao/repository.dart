48a88d442906fa4b3a4f11ab012a40cff942ebc3
julien <julien.luccioni@protonmail.com>
2020-10-05 17:26:26 +0200

    Set up new sqlite

--- lib/dao/repository.dart
+++ lib/dao/repository.dart
@@ -1,5 +1,21 @@
-// https://flutter.dev/docs/cookbook/persistence/sqlite
+import 'package:sqflite/sqflite.dart';
 
-class repository {
+import '../model/todo.dart';
 
+class _DbTodo {
+  static Database _db;
+
+  static Future<Database> get() async {
+    if (_db == null) {
+      var path = await getDatabasesPath();
+      _db = await openDatabase(path + 'todoButInFlutterDb.db');
+    }
+    return _db;
+  }
 }
+
+Future<Todo> getTodos() {}
+
+Future<bool> insertTodo() {}
+
+Future<bool> removeTodo() {}
